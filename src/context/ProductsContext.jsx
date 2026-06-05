ce3d5d7af8273f656c4d5b54dc11d6ec37337be9
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-05 13:27:51 +0330

    products context added

---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { createContext } from 'react'
+import { api } from '../services/api'
+
+const ProductContext = createContext()
+
+const ProductsProvider = ({ children }) => {
+  const [products, setProducts] = useState([])
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        setProducts(await api.get("/products"))
+      } catch (err) {
+        console.log(err.message)
+        alert(err.message)
+      }
+    }
+    getProducts()
+  }, [])
+  return (
+    <ProductContext.Provider value={products}>
+      {children}
+    </ProductContext.Provider>
+  )
+}
+
+export default ProductsProvider
