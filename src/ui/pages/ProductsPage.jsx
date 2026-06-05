68d1cde33ddf22415900110295bc26b0199969e2
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-05 17:53:43 +0330

    search html added

--- src/ui/pages/ProductsPage.jsx
+++ src/ui/pages/ProductsPage.jsx
@@ -3,21 +3,36 @@ import { useProducts } from '../../context/ProductsContext'
 import styles from "./ProductsPage.module.css"
 import Card from '../components/Card'
 import Loader from '../components/Loader'
+import { ImSearch } from 'react-icons/im'
+import { useState } from 'react'
 const ProductsPage = () => {
+  const [search , setSearch] = useState("")
   const products = useProducts()
 
+  const searchHandler = () => {
+    console.log(search)
+  }
+
   return (
-    <div className={styles.container}>
-      <div className={styles.products}>
-        {products.length===0 && <Loader/>}
-        {products?.map(product=>(
-          <Card key={product.id} product={product}/>
-        ))}
+    <>
+      <div>
+        <input type="text" value={search} onChange={(e)=>setSearch(e.target.value.toLocaleLowerCase())}
+        onClick={searchHandler}
+        placeholder='search'/>
+        <button><ImSearch/></button>
       </div>
-      <aside>
-        sidebar
-      </aside>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.products}>
+          {products.length === 0 && <Loader />}
+          {products?.map(product => (
+            <Card key={product.id} product={product} />
+          ))}
+        </div>
+        <aside>
+          sidebar
+        </aside>
+      </div>
+    </>
   )
 }
 
