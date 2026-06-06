cfd0f3d67d84b61be1c8e6e725fc648b014a8265
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-06 19:03:00 +0330

    useCart hook added

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,20 +1,31 @@
 import React from 'react'
+import { useContext } from 'react'
 import { useReducer } from 'react'
 import { createContext } from 'react'
 
 const CartContext = createContext()
 const initialState = {}
-const reducer = () => {}
+const reducer = (state,action) => {
+    console.log(action)
+}
+
+
+
 
 const CartProvider = ({children}) => {
    const [state , dispatch] = useReducer(reducer,initialState)
 
 
     return (
-        <CartContext.Provider value={state}>
+        <CartContext.Provider value={{state,dispatch}}>
             {children}
         </CartContext.Provider>
     )
+}
+
+export const useCart = ()=>{
+   const {state,dispatch} = useContext(CartContext)
+    return [state,dispatch]
 }
 
 export default CartProvider
--- src/ui/components/Card.jsx
+++ src/ui/components/Card.jsx
@@ -3,10 +3,17 @@ import {TbListDetails} from "react-icons/tb"
 import { TbShoppingBagCheck } from "react-icons/tb"
 import { shortenText } from "../../helpers/helper"
 import styles from "./Card.module.css"
+import { useCart } from "../../context/CartContext"
 
 const Card = ({ product }) => {
     const {id,title,image,price} = product
     const shortenedTitle = shortenText(title)
+    const [state,dispatch] = useCart()
+
+    const clickHandler = () => {
+        dispatch({type:"add",payload:product})
+    }
+
     return (
         <div className={styles.card}>
             <img src={image} alt={shortenedTitle} loading="lazy" />
@@ -17,7 +24,7 @@ const Card = ({ product }) => {
                     <TbListDetails/>
                 </Link>
                 <div>
-                <button>
+                <button onClick={clickHandler}>
                     <TbShoppingBagCheck/>
                 </button>
                 </div>
