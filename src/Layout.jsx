4765caa63c290edfa6e408a78088c2e44b7559e7
parsaDokhtMohammadi <parsadokhtmohammadi.work@gmail.com>
2026-06-07 12:47:55 +0330

    layout added

---
+++ src/Layout.jsx
@@ -0,0 +1,26 @@
+import { PiShoppingCartSimpleBold } from "react-icons/pi"
+import { Link } from "react-router"
+import { useCart } from "./context/CartContext"
+import styles from "./Layout.module.css"
+const Layout = ({ children }) => {
+    const [state] = useCart()
+    return (
+        <>
+            <header className={styles.header}>
+                <Link to={"/products"}>PDM Shop</Link>
+                <div>
+                    <Link to={"/checkout"}><PiShoppingCartSimpleBold /></Link>
+                    {!!state.itemsCounter && (
+                        <span>{state.itemsCounter}</span>
+                    )}
+                </div>
+            </header>
+            {children}
+            <footer className={styles.footer}>
+                <p>Developed by <a href="https://github.com/ParsaDokhtMohammadi">Parsa Dokht Mohammadi</a></p>
+            </footer>
+        </>
+    )
+}
+
+export default Layout
